9a68f6274d3f71c736c5fa174c3cf8a0b9efdc7c
Pawel Siwon <pawel.siwon@cdq.com>
2022-04-07 14:53:39 +0200

    reformat

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -8,8 +8,8 @@ public class Game implements IGame
 {
     List<String> players = new ArrayList<>();
     int[] places = new int[6];
-    int[] purses  = new int[6];
-    boolean[] inPenaltyBox  = new boolean[6];
+    int[] purses = new int[6];
+    boolean[] inPenaltyBox = new boolean[6];
 
     List<String> popQuestions = new LinkedList<>();
     List<String> scienceQuestions = new LinkedList<>();
@@ -18,103 +18,117 @@ public class Game implements IGame
 
     int currentPlayer = 0;
     boolean isGettingOutOfPenaltyBox;
-    
-    public  Game(){
-    	for (int i = 0; i < 50; i++) {
-			popQuestions.add("Pop Question " + i);
-			scienceQuestions.add(("Science Question " + i));
-			sportsQuestions.add(("Sports Question " + i));
-			rockQuestions.add(createRockQuestion(i));
-    	}
+
+    public Game()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            popQuestions.add("Pop Question " + i);
+            scienceQuestions.add(("Science Question " + i));
+            sportsQuestions.add(("Sports Question " + i));
+            rockQuestions.add(createRockQuestion(i));
+        }
     }
 
-	public String createRockQuestion(int index){
-		return "Rock Question " + index;
-	}
+    public String createRockQuestion(int index)
+    {
+        return "Rock Question " + index;
+    }
 
-	@Override
-	public boolean add(String playerName) {
-		
-		
-	    players.add(playerName);
-	    places[howManyPlayers()] = 0;
-	    purses[howManyPlayers()] = 0;
-	    inPenaltyBox[howManyPlayers()] = false;
-	    
-	    System.out.println(playerName + " was added");
-	    System.out.println("They are player number " + players.size());
-		return true;
-	}
-	
-	public int howManyPlayers() {
-		return players.size();
-	}
+    @Override
+    public boolean add(String playerName)
+    {
 
-	@Override
-	public void roll(int roll) {
-		System.out.println(players.get(currentPlayer) + " is the current player");
-		System.out.println("They have rolled a " + roll);
-		
-		if (inPenaltyBox[currentPlayer]) {
-			if (roll % 2 != 0) {
-				isGettingOutOfPenaltyBox = true;
-				
-				System.out.println(players.get(currentPlayer) + " is getting out of the penalty box");
-				places[currentPlayer] = places[currentPlayer] + roll;
-				if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
-				
-				System.out.println(players.get(currentPlayer) 
-						+ "'s new location is " 
-						+ places[currentPlayer]);
-				System.out.println("The category is " + currentCategory());
-				askQuestion();
-			} else {
-				System.out.println(players.get(currentPlayer) + " is not getting out of the penalty box");
-				isGettingOutOfPenaltyBox = false;
-				}
-			
-		} else {
-		
-			places[currentPlayer] = places[currentPlayer] + roll;
-			if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
-			
-			System.out.println(players.get(currentPlayer) 
-					+ "'s new location is " 
-					+ places[currentPlayer]);
-			System.out.println("The category is " + currentCategory());
-			askQuestion();
-		}
-		
-	}
+        players.add(playerName);
+        places[howManyPlayers()] = 0;
+        purses[howManyPlayers()] = 0;
+        inPenaltyBox[howManyPlayers()] = false;
 
-	private void askQuestion() {
-		String question;
+        System.out.println(playerName + " was added");
+        System.out.println("They are player number " + players.size());
+        return true;
+    }
 
-		switch (currentCategory())
-		{
-			case "Pop" -> question = popQuestions.remove(0);
-			case "Science" -> question = scienceQuestions.remove(0);
-			case "Sports" -> question = sportsQuestions.remove(0);
-			case "Rock" -> question = rockQuestions.remove(0);
-			default -> throw new IllegalStateException("Unknown category of question " + currentCategory());
-		}
+    public int howManyPlayers()
+    {
+        return players.size();
+    }
 
-		System.out.println(question);
-	}
-	
-	
-	private String currentCategory() {
+    @Override
+    public void roll(int roll)
+    {
+        System.out.println(players.get(currentPlayer) + " is the current player");
+        System.out.println("They have rolled a " + roll);
+
+        if (inPenaltyBox[currentPlayer])
+        {
+            if (roll % 2 != 0)
+            {
+                isGettingOutOfPenaltyBox = true;
+
+                System.out.println(players.get(currentPlayer) + " is getting out of the penalty box");
+                places[currentPlayer] = places[currentPlayer] + roll;
+                if (places[currentPlayer] > 11)
+                    places[currentPlayer] = places[currentPlayer] - 12;
+
+                System.out.println(players.get(currentPlayer)
+                        + "'s new location is "
+                        + places[currentPlayer]);
+                System.out.println("The category is " + currentCategory());
+                askQuestion();
+            }
+            else
+            {
+                System.out.println(players.get(currentPlayer) + " is not getting out of the penalty box");
+                isGettingOutOfPenaltyBox = false;
+            }
+
+        }
+        else
+        {
+
+            places[currentPlayer] = places[currentPlayer] + roll;
+            if (places[currentPlayer] > 11)
+                places[currentPlayer] = places[currentPlayer] - 12;
+
+            System.out.println(players.get(currentPlayer)
+                    + "'s new location is "
+                    + places[currentPlayer]);
+            System.out.println("The category is " + currentCategory());
+            askQuestion();
+        }
+
+    }
+
+    private void askQuestion()
+    {
+        String question;
+
+        switch (currentCategory())
+        {
+            case "Pop" -> question = popQuestions.remove(0);
+            case "Science" -> question = scienceQuestions.remove(0);
+            case "Sports" -> question = sportsQuestions.remove(0);
+            case "Rock" -> question = rockQuestions.remove(0);
+            default -> throw new IllegalStateException("Unknown category of question " + currentCategory());
+        }
+
+        System.out.println(question);
+    }
+
+    private String currentCategory()
+    {
         var mod = places[currentPlayer] % 4;
 
-		return switch (mod)
-				{
-					case 0 -> "Pop";
-					case 1 -> "Science";
-					case 2 -> "Sports";
-					default -> "Rock";
-				};
+        return switch (mod)
+                {
+                    case 0 -> "Pop";
+                    case 1 -> "Science";
+                    case 2 -> "Sports";
+                    default -> "Rock";
+                };
 
-	}
+    }
 
 	@Override
 	public boolean wasCorrectlyAnswered() {
@@ -168,8 +182,8 @@ public class Game implements IGame
 		return true;
 	}
 
-
-	private boolean didPlayerWin() {
-		return !(purses[currentPlayer] == 6);
-	}
+    private boolean didPlayerWin()
+    {
+        return !(purses[currentPlayer] == 6);
+    }
 }
