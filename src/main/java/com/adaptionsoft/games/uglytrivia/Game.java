58327b4305f9059ff6e5abab33d60b9637597bb9
Pawel Siwon <pawel.siwon@cdq.com>
2022-04-07 14:53:02 +0200

    refactor: listy, switche

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -2,37 +2,34 @@ package com.adaptionsoft.games.uglytrivia;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Game implements IGame
 {
-    ArrayList players = new ArrayList();
+    List<String> players = new ArrayList<>();
     int[] places = new int[6];
     int[] purses  = new int[6];
     boolean[] inPenaltyBox  = new boolean[6];
-    
-    LinkedList popQuestions = new LinkedList();
-    LinkedList scienceQuestions = new LinkedList();
-    LinkedList sportsQuestions = new LinkedList();
-    LinkedList rockQuestions = new LinkedList();
-    
+
+    List<String> popQuestions = new LinkedList<>();
+    List<String> scienceQuestions = new LinkedList<>();
+    List<String> sportsQuestions = new LinkedList<>();
+    List<String> rockQuestions = new LinkedList<>();
+
     int currentPlayer = 0;
     boolean isGettingOutOfPenaltyBox;
     
     public  Game(){
     	for (int i = 0; i < 50; i++) {
-			popQuestions.addLast("Pop Question " + i);
-			scienceQuestions.addLast(("Science Question " + i));
-			sportsQuestions.addLast(("Sports Question " + i));
-			rockQuestions.addLast(createRockQuestion(i));
+			popQuestions.add("Pop Question " + i);
+			scienceQuestions.add(("Science Question " + i));
+			sportsQuestions.add(("Sports Question " + i));
+			rockQuestions.add(createRockQuestion(i));
     	}
     }
 
 	public String createRockQuestion(int index){
 		return "Rock Question " + index;
-	}
-	
-	public boolean isPlayable() {
-		return (howManyPlayers() >= 2);
 	}
 
 	@Override
@@ -91,28 +88,32 @@ public class Game implements IGame
 	}
 
 	private void askQuestion() {
-		if (currentCategory() == "Pop")
-			System.out.println(popQuestions.removeFirst());
-		if (currentCategory() == "Science")
-			System.out.println(scienceQuestions.removeFirst());
-		if (currentCategory() == "Sports")
-			System.out.println(sportsQuestions.removeFirst());
-		if (currentCategory() == "Rock")
-			System.out.println(rockQuestions.removeFirst());		
+		String question;
+
+		switch (currentCategory())
+		{
+			case "Pop" -> question = popQuestions.remove(0);
+			case "Science" -> question = scienceQuestions.remove(0);
+			case "Sports" -> question = sportsQuestions.remove(0);
+			case "Rock" -> question = rockQuestions.remove(0);
+			default -> throw new IllegalStateException("Unknown category of question " + currentCategory());
+		}
+
+		System.out.println(question);
 	}
 	
 	
 	private String currentCategory() {
-		if (places[currentPlayer] == 0) return "Pop";
-		if (places[currentPlayer] == 4) return "Pop";
-		if (places[currentPlayer] == 8) return "Pop";
-		if (places[currentPlayer] == 1) return "Science";
-		if (places[currentPlayer] == 5) return "Science";
-		if (places[currentPlayer] == 9) return "Science";
-		if (places[currentPlayer] == 2) return "Sports";
-		if (places[currentPlayer] == 6) return "Sports";
-		if (places[currentPlayer] == 10) return "Sports";
-		return "Rock";
+        var mod = places[currentPlayer] % 4;
+
+		return switch (mod)
+				{
+					case 0 -> "Pop";
+					case 1 -> "Science";
+					case 2 -> "Sports";
+					default -> "Rock";
+				};
+
 	}
 
 	@Override
